5678f4b8da96e0cef545855b8142822194ebb616
Kaue-G <kaueguedes.dev@gmail.com>
2022-08-11 11:06:53 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { Header } from "./components/Header"
-import './global.css'
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import './global.css';
+import styles from './App.module.css';
 
 function App() {
 
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lorem"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eveniet dolores neque ut aliquid asperiores ad id culpa, voluptatem aspernatur ea eos sapiente, debitis tempora consequatur quo nostrum vel? Ratione."
+          />
+          <Post
+            author="Fernandes"
+            content="new post"
+          />
+        </main>
+      </div>
     </>
   )
-}
+};
 
-export default App
+export default App;
