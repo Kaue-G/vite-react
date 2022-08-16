b7f0690f0cad82702f791df23436c584f2db4cc9
Kaue-G <kaueguedes.dev@gmail.com>
2022-08-16 14:58:46 -0300

    hotFix

--- tsx/src/components/Header/index.tsx
+++ tsx/src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import styles from './index.module.css';
 import joystickLogo from '../../assets/joystick-logo.svg';
+import { GameController } from 'phosphor-react';
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <img src={joystickLogo} alt="Logotipo de joystick" />
+            <GameController size={50} />
         </header>
     );
 };
